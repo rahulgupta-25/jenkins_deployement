553a79b0b81ed1149d1fdd64c6a64bc9c4ceb381
rahulgupta-25 <r3408g@gmail.com>
2026-05-08 12:11:37 +0530

    Update backend.groovy

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Code-pull') {
             steps {
-                git branch: 'main', credentialsId: '2b26b3e0-bceb-42f9-9fa2-cf95c1c91672', url: 'https://github.com/rahulgupta-25/jenkins_deployement.git'
+                git branch: 'main', credentialsId: 'github-creds', url: 'https://github.com/rahulgupta-25/jenkins_deployement.git'
             }
         }
         stage('Build') {
